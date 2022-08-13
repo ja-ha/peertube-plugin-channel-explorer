b5e3a329ae1ed784135d87276fc88f3a95d6f67a
Bucaille Thommy <contact@bucaille-thommy.fr>
2022-08-13 22:43:13 +0200

    Admin stats plugin

--- peertube-plugin-orion-admin-stats/client/common-client-plugin.js
+++ peertube-plugin-orion-admin-stats/client/common-client-plugin.js
@@ -1,178 +1,49 @@
-const PREVIOUS_STORAGE_KEY = 'orionPreviousFieldsValues';
-const defaultPreviousFieldsValues = {
-  _category: '',
-  description: '',
-  _language: '',
-  _licence: '',
-  nsfw: '',
-  _privacy: '',
-};
-const tempSelectValue = {};
+const AdminPage = require("./pages/admin");
 
-function register({ registerHook, peertubeHelpers }) {
+async function register({
+  registerHook,
+  peertubeHelpers,
+  registerClientRoute,
+}) {
+  
+  /**
+   * Add link admin page
+   */
   registerHook({
-    target: 'action:video-edit.init',
-    handler: ({ type }) => {
-      if(type == "update")
-        return;
+    target: "action:router.navigation-end",
+    handler: async (params) => {
+      if (params.path.startsWith("/admin/")) {
+        if (document.getElementById("admin-stats-link")) return;
 
-      const timer = setInterval(() => {
-        // Wait for hidden not exist
-        if(!document.querySelector('form').hasAttribute('hidden')) {
-          clearInterval(timer);
-          setTimeout(init, 500);
-        }
-      }, 300);
-    }
-  })
-}
+        let href = "/p/admin/stats";
 
-export {
-  register
-}
+        // Get menu container
+        const menuContainer = document.getElementsByClassName("sub-menu")[0];
 
-function init() {
-  fillFormFields();
+        // Create link
+        const content = `
+          <a _ngcontent-dke-c79="" id="admin-stats-link" routerlinkactive="active" class="title-page title-page-settings ng-star-inserted" href="${href}">
+            ${await peertubeHelpers.translate("Statistics")}
+          </a>
+        `;
 
-  const publishButton = document.querySelector("[label='Publier']");
-  publishButton.addEventListener('click', onSubmitSaveFieldsValues);
+        // Create node for it
+        const nodeLink = document.createElement("div");
+        nodeLink.innerHTML = content.trim();
 
-  // Listen hover all select fields
-  const selects = Object.keys(defaultPreviousFieldsValues).filter(fieldName => fieldName.startsWith('_'));
-  selects.forEach(select => {
-    const field = document.getElementById(select.substring(1));
-    field.parentNode.parentNode.addEventListener('click', () => {
-      setTimeout(() => {
-        const selectOpenedOptions = field.parentNode.parentNode.parentNode.nextSibling.childNodes[1].childNodes[1];
-        if(selectOpenedOptions) {
-          // For each child, click event
-          selectOpenedOptions.childNodes.forEach(child => {
-            if(!child || !child.id) return;
-
-            child.addEventListener('click', () => {
-              let id = child.id.split("-")[1];
-              tempSelectValue[select] = id;
-            });
-          });
-        }
-      }, 100);
-    });
-  });
-}
-
-function onSubmitSaveFieldsValues() {
-  const form = document.querySelector('form');
-  const fields = form.querySelectorAll('input, textarea');
-  const fieldsValues = {};
-  fields.forEach(field => {
-    if(!field || !field.id) return;
-
-    const availableOptions = Object.keys(defaultPreviousFieldsValues).map(fieldName => fieldName.startsWith('_') ? fieldName.substring(1) : fieldName);
-
-    // Check if field exist in default values
-    if (availableOptions.includes(field.id)) {
-      // Search real option name
-      const fieldName = Object.keys(defaultPreviousFieldsValues).find(fieldName => fieldName === field.id || fieldName === `_${field.id}`);
-
-      // Checkbox
-      if (field.type === 'checkbox') {
-        fieldsValues[fieldName] = field.checked;
-
-      }else if (fieldName.startsWith('_')) {
-        fieldsValues[fieldName] = tempSelectValue[fieldName];
-
-      // Other fields
-      } else {
-        fieldsValues[fieldName] = field.value;
+        // Insert to menu container
+        menuContainer.appendChild(nodeLink.firstChild);
       }
-      
-    }
+    },
   });
 
-  localStorage.setItem(PREVIOUS_STORAGE_KEY, JSON.stringify(fieldsValues));
-}
-
-
-function getPreviousFieldsValues() {
-  // replace default values with previous values
-  const previousFieldsValues = JSON.parse(localStorage.getItem(PREVIOUS_STORAGE_KEY)) || defaultPreviousFieldsValues;
-
-  // Return values
-  return previousFieldsValues;
-}
-
-function fillFormFields() {
-  // Get previous values
-  const previousFieldsValues = getPreviousFieldsValues();
-
-  // Fill form fields
-  let wait = false;
-  Object.keys(previousFieldsValues).forEach(fieldName => {
-    const fieldId = fieldName;
-
-    let isSelect = false;
-    if(fieldName.startsWith('_')) {
-      fieldName = fieldName.substring(1);
-      isSelect = true;
-    }
-
-    const field = document.getElementById(fieldName);
-
-    if (field) {
-      // Checkbox
-      if (typeof previousFieldsValues[fieldId] === 'boolean') {
-        field.checked = previousFieldsValues[fieldId];
-
-      // Select
-      }else if (isSelect) {
-        if(!previousFieldsValues[fieldId]) return;
-
-        const doJob = (field) => {
-          if(wait) {
-            setTimeout(() => doJob(field), 1000);
-            return;
-          }
-
-          wait = true;
-          field.parentNode.parentNode.parentNode.getElementsByClassName("ng-arrow")[0].dispatchEvent(new CustomEvent('mousedown', { bubbles: true }))
-          setTimeout(() => {
-            try {
-              const selectOpenedOptions = field.parentNode.parentNode.parentNode.nextSibling.childNodes[1].childNodes[1];
-              if(selectOpenedOptions) {
-                for(let i = 0; i < selectOpenedOptions.childNodes.length; i++) {
-                  const child = selectOpenedOptions.childNodes[i];
-                  if(!child || !child.id) return;
-  
-                  let id = child.id.split("-")[1];
-                  if(id == previousFieldsValues[fieldId]) {
-                    tempSelectValue[fieldId] = id;
-                    child.click();
-                    break;
-                  }
-                }
-  
-                setTimeout(() => {
-                  wait = false;
-                }, 500);
-              }
-            }
-            catch(e) {
-              wait = false;
-              doJob(field);
-            }
-          }, 500);
-        };
-
-        doJob(field);
-
-      // Other fields
-      }else{
-        if(!previousFieldsValues[fieldId]) return;
-        field.value = previousFieldsValues[fieldId];
-      }
-
-      field.dispatchEvent(new Event('input'));
-      field.dispatchEvent(new Event('change'));
-    }
+  // Register the admin stats route
+  registerClientRoute({
+    route: "admin/stats",
+    onMount: ({ rootEl }) => {
+      AdminPage.showPage({rootEl, peertubeHelpers});
+    },
   });
 }
+
+export { register };
--- peertube-plugin-orion-admin-stats/main.js
+++ peertube-plugin-orion-admin-stats/main.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 
 async function register({
   registerHook,
@@ -10,7 +11,148 @@ async function register({
   getRouter,
   peertubeHelpers
 }) {
-  
+  const router = getRouter();
+
+  router.get("/stats", async (req, res) => {
+    
+    try {
+      const { from, to, groupBy } = req.query;
+      const fromDate = moment(from);
+      const toDate = moment(to);
+      const startOfMonth = moment().startOf('month');
+      
+      let usersCount = 0;
+      let usersThisMonth;
+      let videosCount = 0;
+      let videosThisMonth = 0;
+      let openAbusesCount = 0;
+      let videoViewsStats = [];
+
+      // Users count
+      let request = await peertubeHelpers.database.query(
+        'SELECT COUNT(*) AS "count" FROM "user"',
+        {
+          type: "SELECT"
+        }
+      );
+      
+      if(request && request.length > 0)
+        usersCount = request[0].count;
+
+      // Users this month (createdAt)
+      request = await peertubeHelpers.database.query(
+        'SELECT COUNT(*) AS "count" FROM "user" WHERE "createdAt" > $dat',
+        {
+          type: "SELECT",
+          bind: { dat: startOfMonth.format('YYYY-MM-DD HH:mm:ss') }
+        }
+      );
+
+      if(request && request.length > 0)
+        usersThisMonth = request[0].count;
+      
+      // Videos count
+      request = await peertubeHelpers.database.query(
+        'SELECT COUNT(*) AS "count" FROM "video"',
+        {
+          type: "SELECT"
+        }
+      );
+      
+      if(request && request.length > 0)
+        videosCount = request[0].count;
+
+      // Videos count this month
+      request = await peertubeHelpers.database.query(
+        'SELECT COUNT(*) AS "count" FROM "video" WHERE "createdAt" > $dat',
+        {
+          type: "SELECT",
+          bind: { dat: startOfMonth.format('YYYY-MM-DD HH:mm:ss') }
+        }
+      );
+
+      if(request && request.length > 0)
+        videosThisMonth = request[0].count;
+
+      // Open abuse count
+      request = await peertubeHelpers.database.query(
+        'SELECT COUNT(*) AS "count" FROM "abuse" WHERE "state" = $state',
+        {
+          type: "SELECT",
+          bind: { state: "1" }
+        }
+      );
+
+      if(request && request.length > 0)
+        openAbusesCount = request[0].count;
+
+      // Video views stats (using fromDate, toDate)
+      request = await peertubeHelpers.database.query(
+        'SELECT "videoId", "views", "startDate", "endDate" FROM "videoView" WHERE "startDate" >= $start AND "endDate" <= $end',
+        {
+          type: "SELECT",
+          bind: { start: fromDate.format('YYYY-MM-DD HH:mm:ss'), end: toDate.format('YYYY-MM-DD HH:mm:ss') }
+        }
+      );
+
+      if(request && request.length > 0) {
+        // Group stats video views by groupBy (day, month, year)
+        let groupedStats = {};
+        for(let i = 0; i < request.length; i++) {
+          let format = "YYYY-MM-DD";
+          if(groupBy === "month")
+            format = "YYYY-MM";
+          else if(groupBy === "year")
+            format = "YYYY";
+          
+
+          const videoView = request[i];
+          const date = moment(videoView.startDate).format(format);
+          const dateKey = date;
+          const videoKey = videoView.videoId;
+
+          if(!groupedStats[dateKey]) {
+            groupedStats[dateKey] = {};
+          }
+
+          if(!groupedStats[dateKey][videoKey]) {
+            groupedStats[dateKey][videoKey] = {
+              views: 0
+            };
+          }
+
+          groupedStats[dateKey][videoKey].views += videoView.views;
+        }
+
+        // Convert grouped stats to array
+        videoViewsStats = Object.keys(groupedStats).map(key => {
+          return {
+            date: key,
+            items: Object.keys(groupedStats[key]).map(videoKey => {
+              return {
+                videoId: videoKey,
+                views: groupedStats[key][videoKey].views
+              };
+            })
+          };
+        });
+
+        // Sort video views stats by date
+        videoViewsStats.sort((a, b) => {
+          return moment(a.date).isAfter(moment(b.date)) ? 1 : -1;
+        });
+      }
+      
+      res.json({
+        status: "success",
+        data: { usersCount, usersThisMonth, videosCount, videosThisMonth, openAbusesCount, videoViewsStats }
+      });
+
+    } catch (error) {
+      peertubeHelpers.logger.error(error.message, { error });
+      res.status(500).send(error.message);
+    }
+  });
 }
 
 async function unregister() {
