ad894fd15a9f1e392317679d8544063ccf80086f
Bucaille Thommy <contact@bucaille-thommy.fr>
2022-08-15 02:50:13 +0200

    Fixe bugs with ad player

--- peertube-plugin-orion-monetization/client/shared-player.js
+++ peertube-plugin-orion-monetization/client/shared-player.js
@@ -9,6 +9,19 @@ export async function buildPlayer(peertubeHelpers, video, player, videojs) {
         return;
     }
 
+    // Do not show ad one time of 2 (check local storage)
+    const adAlreadShow = localStorage.getItem("ad-already-show") || false;
+    if(adAlreadShow) {
+        // Remove item in local storage to see ad the next time
+        localStorage.removeItem("ad-already-show");
+        console.log("Ad already show, skip next.");
+        return;
+    }else{
+        // Set item in local storage to not see ad the next time
+        localStorage.setItem("ad-already-show", true);
+    }
+
+
     // Get settings
     const settings = await peertubeHelpers.getSettings();
     const adsEnabled = await settings['enable-video-ads'];
@@ -26,7 +39,10 @@ export async function buildPlayer(peertubeHelpers, video, player, videojs) {
     const zoneID = await settings['craftyourads-zone-id'];
     const api = `https://manager.craftyourads.com/adserve?zone_id=${zoneID}&type=json`;
     await player.ready();
-    const base_src = player.currentSrc();
+    const base_src = {
+        src: player.currentSrc(),
+        type: player.currentType()
+    }
 
     // Get video ads
     try {
@@ -37,9 +53,13 @@ export async function buildPlayer(peertubeHelpers, video, player, videojs) {
         const redirectUrl = data.redirect_url;
 
         player.src(videoUrl);
-        player.controls(false);
         player.currentTime(0);
-        player.play();
+        player.controls(false);
+
+        
+        await player.play().catch(() => {
+            console.log("Error while auto playing video. Waiting for user runnning the video.");
+        });
 
         addAdsSkipTimer(peertubeHelpers, player, base_src, redirectUrl, adsDuration, video);
     }
@@ -49,23 +69,33 @@ export async function buildPlayer(peertubeHelpers, video, player, videojs) {
 }
 
 async function addAdsSkipTimer(peertubeHelpers, player, fallback_src, creative_url, adsDuration, video) {
-    const skipButtonContainer = document.getElementById('plugin-placeholder-player-next');
+    const skipButtonContainer = document.getElementById('video-wrapper');
+    // Set as relative position
+    skipButtonContainer.style.position = 'relative';
 
-    // Add block button after #plugin-placeholder-player-next
+    // Create a container for our buttons
+    const skipButtonContainerDiv = document.createElement('div');
+    skipButtonContainerDiv.style.position = 'absolute';
+    skipButtonContainerDiv.style.bottom = '15px';
+    skipButtonContainerDiv.style.right = '15px';
+    skipButtonContainerDiv.style.color = '#fff';
+    skipButtonContainerDiv.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    skipButtonContainerDiv.style.padding = '5px';
+    skipButtonContainerDiv.style.borderRadius = '5px';
+    skipButtonContainerDiv.innerHTML = "<p>" + await peertubeHelpers.translate("Your are seeing this Ad from CraftYourAds") + "</p>";
+
+    // Add block button after #video-wrapper
     const skipButton = document.createElement('button');
     skipButton.id = 'skip-button';
     skipButton.innerText = await peertubeHelpers.translate('Skip Ad');
     skipButton.classList.add('btn', 'btn-primary', 'btn-sm');
     skipButton.style.display = 'none';
 
-    skipButtonContainer.appendChild(skipButton);
-
     // Add skip timer
     const skipTimer = document.createElement('button');
     skipTimer.id = 'skip-timer';
-    skipTimer.innerHTML = await peertubeHelpers.translate('Skip Ad in') + "<br>" + adsDuration + ' ' + await peertubeHelpers.translate('seconds');
+    skipTimer.innerHTML = await peertubeHelpers.translate('Skip Ad in') + " " + adsDuration + ' ' + await peertubeHelpers.translate('seconds');
     skipTimer.classList.add('btn', 'btn-primary', 'btn-sm');
-    skipButtonContainer.appendChild(skipTimer);
     skipTimer.addEventListener('click', () => {
         window.open(creative_url, '_blank');
     });
@@ -73,11 +103,52 @@ async function addAdsSkipTimer(peertubeHelpers, player, fallback_src, creative_u
     // Add creative button 
     const creativeButton = document.createElement('button');
     creativeButton.id = 'creative-button';
-    creativeButton.innerText = await peertubeHelpers.translate('View more') + '>>';
+    creativeButton.innerText = await peertubeHelpers.translate('View more') + ' >>';
     creativeButton.classList.add('btn', 'btn-primary', 'btn-sm');
-    skipButtonContainer.appendChild(creativeButton);
     creativeButton.addEventListener('click', () => {
         window.open(creative_url, '_blank');
+    });
+
+    // Add our buttons to our container
+    skipButtonContainerDiv.appendChild(skipButton);
+    skipButtonContainerDiv.appendChild(skipTimer);
+    skipButtonContainerDiv.appendChild(creativeButton);
+
+    // Add our container to main container
+    skipButtonContainer.appendChild(skipButtonContainerDiv);
+
+    // Function to skip the ad
+    const skipNow = async (viewed = true) => {
+        player.src({
+            src: fallback_src.src,
+            type: fallback_src.type
+        });
+        player.preload('auto');
+        player.controls(true);
+        player.off("playing");
+        player.currentTime(0);
+        player.play();
+
+        // Remove skip button
+        skipButton.remove();
+        creativeButton.remove();
+        skipButtonContainerDiv.remove();
+
+        // Ping ads view
+        if(viewed) {
+            const account = video.account.userId;
+            const baseUrl = peertubeHelpers.getBaseRouterRoute();
+            await fetch(`${baseUrl}/ping-ads?accountId=${account}&video=${video.id}`, {
+                method: 'GET'
+            });
+        }
+    };
+
+    // On player error, skip ad
+    player.on('error', (err) => {
+        player.off('error');
+        console.log(err);
+        skipNow(false);
     });
 
     // Add interval to update skip timer
@@ -87,6 +158,18 @@ async function addAdsSkipTimer(peertubeHelpers, player, fallback_src, creative_u
         const currentTime = player.currentTime();
         const remainingTime = duration - currentTime;
 
+        if(player.ended()) {
+            // Video ended, skip ad
+            skipNow();
+            return;
+        }
+
+        if(player.paused()) {
+            player.controls(true);
+        }else{
+            player.controls(false);
+        }
+
         if(remainingTime <= 0) {
             // Stop interval
             clearInterval(interval);
@@ -94,29 +177,17 @@ async function addAdsSkipTimer(peertubeHelpers, player, fallback_src, creative_u
             skipButton.style.display = 'inline-block';
 
             // Add click event to skip button
-            skipButton.addEventListener('click', async () => {
-                player.controls(true);
-                player.src(fallback_src);
-                player.play();
-
-                // Remove skip button
-                skipButton.remove();
-                creativeButton.remove();
-
-                // Ping ads view
-                const account = video.account.userId;
-                const baseUrl = peertubeHelpers.getBaseRouterRoute();
-                await fetch(`${baseUrl}/ping-ads?accountId=${account}&video=${video.id}`, {
-                    method: 'GET'
-                });
+            skipButton.addEventListener('click', () => {
+                skipNow();
             });
 
             return;
         }
 
+        // Update skip timer
         const minutes = Math.floor(remainingTime / 60);
         const seconds = Math.floor(remainingTime % 60);
-        const formattedTime = await peertubeHelpers.translate("Skip Ad in") + "<br>" + `${minutes}:${seconds}`;
+        const formattedTime = await peertubeHelpers.translate("Skip Ad in") + " " + `${minutes}:${seconds}`;
         skipTimer.innerHTML = formattedTime;
     } , 1000);
 }
